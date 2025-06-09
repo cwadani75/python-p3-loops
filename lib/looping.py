da24ac15864094a75809efafff06c6090d5ccbaa
cwadani75 <cwadani75@gmail.com>
2025-06-09 14:29:12 +0300

    labs-loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    # Countdown from 10 to 1 and then print "Happy New Year!"
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # Return a new list containing the squares of the integers
+    return [x ** 2 for x in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    # Print numbers from 1 to 100
+    # For multiples of 3 print "Fizz" instead of the number
+    # For multiples of 5 print "Buzz"
+    # For multiples of both 3 and 5 print "FizzBuzz"
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
